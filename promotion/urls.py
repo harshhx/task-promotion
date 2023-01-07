aaeeeae3fe81add164191051a0372590027507ca
harshhx <hg070401@gmail.com>
2023-01-08 01:53:16 +0530

    create promotion

--- promotion/urls.py
+++ promotion/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import Welcome, CreatePlan
+from .views import Welcome, CreatePlan, CreatePromotion
 urlpatterns = [
     path('', Welcome.as_view()),
-    path('create-plan/', CreatePlan.as_view())
+    path('create-plan/', CreatePlan.as_view()),
+    path('create-promotion/', CreatePromotion.as_view())
 ]
--- promotion/views.py
+++ promotion/views.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from .models import Plan, Promotion
 
 
@@ -15,7 +17,8 @@ class Welcome(APIView):
 
 class CreatePlan(APIView):
 
-    def post(self, request):
+    @staticmethod
+    def post(request):
 
         name = request.data.get('name')
         amount_options = request.data.get('amountOptions')
@@ -57,3 +60,71 @@ class CreatePlan(APIView):
             }
             return Response(data, status.HTTP_200_OK)
 
+
+class CreatePromotion(APIView):
+    @staticmethod
+    def post(request):
+        name = request.data.get("name")
+        plan_id = request.data.get('plan_id')
+        validity_type = request.data.get('validity_type')
+        start_date = request.data.get('start_date')
+        end_date = request.data.get('end_date')
+        users_left = request.data.get('users_left')
+        benefit_percentage = request.data.get('benefit_percentage')
+
+        if not name or not plan_id or not benefit_percentage or (validity_type not in ['end_date', 'num_of_users']):
+            data = {
+                'success': False,
+                'msg': 'Missing name, plan, validity or benefitPercentage',
+                'data': {}
+            }
+            return Response(data, status.HTTP_200_OK)
+
+        if validity_type == 'end_date':
+            if not start_date or not end_date:
+                data = {
+                    'success': False,
+                    'msg': 'Enter start Date and End Date',
+                    'data': {}
+                }
+                return Response(data, status.HTTP_200_OK)
+            else:
+                start_date = datetime.strptime(start_date, '%d-%m-%Y').date()
+                end_date = datetime.strptime(end_date, '%d-%m-%Y').date()
+
+        if validity_type == 'num_of_users':
+            if not users_left:
+                data = {
+                    'success': False,
+                    'msg': 'Missing number of users',
+                    'data': {}
+                }
+                return Response(data, status.HTTP_200_OK)
+        plan = Plan.objects.filter(id=plan_id)
+        if not plan:
+            return Response({
+                'success': False,
+                'msg': 'No Such plan Exists',
+                'data': {}
+            }, status.HTTP_200_OK)
+        plan = plan[0]
+
+        new_promotion = Promotion(
+            name=name,
+            plan=plan,
+            validity_type=validity_type,
+            benefitPercentage=benefit_percentage,
+            start_date=start_date,
+            end_date=end_date,
+            users_left=users_left
+        )
+        new_promotion.save()
+        data = {
+            'success': True,
+            'msg': 'Promotion Created Successfully',
+            'data': {
+                'id': new_promotion.id,
+                'name': new_promotion.name
+            }
+        }
+        return Response(data, status.HTTP_200_OK)
